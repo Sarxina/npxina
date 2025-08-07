20845808a9b6da225bf4092db4e28e104f47ec7d
Sarxina <sarxinavt@gmail.com>
2025-08-07 14:27:14 -0400

    More base classes

--- src/data/classData.ts
+++ src/data/classData.ts
@@ -18,7 +18,7 @@ export const warrior: ClassProfile = {
         major: 'STR',
         minor1: 'CON',
         minor2: 'WIS',
-        dump: 'DEX'
+        dump: 'INT'
     }
 }
 
@@ -64,8 +64,96 @@ export const rogue: ClassProfile = {
     }
 }
 
+export const trickster: ClassProfile = {
+    key: "trickster",
+    selectionText: "a duplicitous trickster",
+    classMod: {
+        coreStatMods: {
+            ac: -1,
+            hp: 0,
+            atk: 0,
+            dpr: 0,
+            dc: 1,
+        },
+        actionMods: []
+    },
+    abilityProfile: {
+        major: 'CHA',
+        minor1: 'DEX',
+        minor2: 'CON',
+        dump: 'STR'
+    }
+}
+
+export const dualist: ClassProfile = {
+    key: "dualist",
+    selectionText: "a adept dualist",
+    classMod: {
+        coreStatMods: {
+            ac: 0,
+            hp: -1,
+            atk: 1,
+            dpr: 0,
+            dc: 0,
+        },
+        actionMods: []
+    },
+    abilityProfile: {
+        major: 'STR',
+        minor1: 'DEX',
+        minor2: 'WIS',
+        dump: 'CHA'
+    }
+}
+
+export const priest: ClassProfile = {
+    key: "priest",
+    selectionText: "a pious priest",
+    classMod: {
+        coreStatMods: {
+            ac: 0,
+            hp: 0,
+            atk: 0,
+            dpr: 0,
+            dc: 0,
+        },
+        actionMods: []
+    },
+    abilityProfile: {
+        major: 'WIS',
+        minor1: 'CON',
+        minor2: 'INT',
+        dump: 'STR'
+    }
+}
+
+export const mage: ClassProfile = {
+    key: "mage",
+    selectionText: "a learned spellcaster",
+    classMod: {
+        coreStatMods: {
+            ac: -1,
+            hp: 0,
+            atk: 0,
+            dpr: 0,
+            dc: 1,
+        },
+        actionMods: []
+    },
+    abilityProfile: {
+        major: 'INT',
+        minor1: 'CON',
+        minor2: 'WIS',
+        dump: 'STR'
+    }
+}
+
 export const classProfiles: ClassProfile[] = [
     warrior,
     archer,
-    rogue
+    rogue,
+    trickster,
+    dualist,
+    priest,
+    mage
 ]
